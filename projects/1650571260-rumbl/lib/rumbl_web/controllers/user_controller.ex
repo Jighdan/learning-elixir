36d32d4561831abbc8f30610c0282ceea17aafff
Reinny Almonte <reinny.almonte@pm.me>
2022-04-25 10:52:21 -0400

    25-04-2022: [project] rumbl -> chapter 5

--- projects/1650571260-rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ projects/1650571260-rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -3,6 +3,8 @@ defmodule RumblWeb.UserController do
 
   alias Rumbl.Accounts
 
+  plug :authenticate when action in [:index, :show]
+
   def index(conn, _params) do
     users = Accounts.get_all()
     render(conn, "index.html", users: users)
@@ -14,19 +16,31 @@ defmodule RumblWeb.UserController do
   end
 
   def new(conn, _params) do
-    changeset = Accounts.change(%Accounts.User{})
+    changeset = Accounts.change_registration(%Accounts.User{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"user" => user_params}) do
-    case Accounts.create(user_params) do
+    case Accounts.register_user(user_params) do
       {:ok, user} ->
         conn
+        |> RumblWeb.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: Routes.user_path(conn, :index))
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access this page")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
     end
   end
 end
