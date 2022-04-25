4b34507019ef3b163dc0378c6feb816d31ad7f99
Reinny Almonte <reinny.almonte@pm.me>
2022-04-25 15:01:40 -0400

    25-04-2022: [project] rumbl -> chapter 6

--- projects/1650571260-rumbl/lib/rumbl_web.ex
+++ projects/1650571260-rumbl/lib/rumbl_web.ex
@@ -23,6 +23,7 @@ defmodule RumblWeb do
 
       import Plug.Conn
       import RumblWeb.Gettext
+      import RumblWeb.Auth, only: [authenticate_user: 2]
       alias RumblWeb.Router.Helpers, as: Routes
     end
   end
@@ -74,6 +75,7 @@ defmodule RumblWeb do
       import Plug.Conn
       import Phoenix.Controller
       import Phoenix.LiveView.Router
+      import RumblWeb.Auth, only: [authenticate_user: 2]
     end
   end
 
