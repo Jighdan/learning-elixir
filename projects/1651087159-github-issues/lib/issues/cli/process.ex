016e2b51fdce2414acaac6e7881285ebc36c9e95
Reinny Almonte <reinny.almonte@pm.me>
2022-04-28 08:07:00 -0400

    27-04-2022: [project] github-issues -> progress

--- projects/1651087159-github-issues/lib/issues/cli/process.ex
+++ projects/1651087159-github-issues/lib/issues/cli/process.ex
@@ -1,5 +1,6 @@
 defmodule Issues.CLI.Process do
   alias Issues.GithubIssues
+  alias Issues.GithubIssues.DataProcess
   alias Issues.CLI.Decoder
 
   def process(:help) do
@@ -11,8 +12,10 @@ defmodule Issues.CLI.Process do
     System.halt()
   end
 
-  def process({user, project, _count}) do
+  def process({user, project, count}) do
     GithubIssues.fetch(user, project)
     |> Decoder.decode_response()
+    |> DataProcess.sort_into_descending_order()
+    |> DataProcess.take(count)
   end
 end
--- projects/README.md
+++ projects/README.md
@@ -7,6 +7,7 @@ A list of the developed projects through the learning.
 1. [cards](./1649950706-cards/)
 2. [identicon](./1649951475-identicon/)
 3. [rumbl](./1650571260-rumbl/)
+4. [github issues](./1651087159-github-issues/)
 
 ## Convention
 
